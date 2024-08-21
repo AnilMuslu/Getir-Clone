576abeaff37de255c4c792553edf7503752deb70
Anıl Muslu <74317226+AnilMuslu@users.noreply.github.com>
2024-08-21 03:12:26 +0300

    Icons and Bottom Bar added

---
+++ GetirClone/app/src/main/java/com/example/getirclone/GetirBottomBar.kt
@@ -0,0 +1,37 @@
+package com.example.getirclone
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GetirBottomBar(){
+    val selectedItem = remember { mutableStateOf(0) }
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar() {
+
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
